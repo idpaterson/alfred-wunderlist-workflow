204df1b6a8a7f5a29e4998e5bec5d34bc7169b05
ipaterson <ipaterson@newstex.com>
2015-06-16 22:02:36 -0400

    Normalize hashtags by removing non-word trailing characters (usually punctuation)

--- src/wunderlist/models/hashtag.py
+++ src/wunderlist/models/hashtag.py
@@ -2,11 +2,11 @@ import re
 from peewee import *
 from wunderlist.models.base import BaseModel
 
-# Wunderlist includes any and all non-whitespace characters in a hashtag,
-# requiring only that the hash be preceded by a space or come at the beginning
-# of a string
 _hashtag_pattern = r'(?<=\s)#\S+'
 _hashtag_pattern_bos = r'^#\S+'
+
+# Remove any non-word characters at the end of the hashtag
+_hashtag_trim_pattern = r'\W+$'
 
 class Hashtag(BaseModel):
 	id = PrimaryKeyField()
@@ -17,8 +17,6 @@ class Hashtag(BaseModel):
 		hashtags = set(re.findall(_hashtag_pattern, task.title) + re.findall(_hashtag_pattern_bos, task.title))
 
 		if len(hashtags) > 0:
-			hashtag_data = [{u'tag': tag} for tag in hashtags]
-
-			print cls.insert_many(hashtag_data).upsert(upsert=True)
+			hashtag_data = [{'tag': re.sub(_hashtag_trim_pattern, r'', tag, flags=re.UNICODE)} for tag in hashtags]
 
 			cls.insert_many(hashtag_data).upsert(True).execute()
