d1b47b41cdd4f71221eef3e1bd754e71c74b0ecd
ipaterson <ipaterson@newstex.com>
2015-12-20 20:14:40 -0500

    Added Wunderlist preference syncing based on the currently undocumented settings API route.

--- src/wunderlist/models/preferences.py
+++ src/wunderlist/models/preferences.py
@@ -4,10 +4,27 @@ from wunderlist.util import workflow
 REMINDER_TIME_KEY = 'reminder_time'
 ICON_THEME_KEY = 'icon_theme'
 EXPLICIT_KEYWORDS_KEY = 'explicit_keywords'
+AUTOMATIC_REMINDERS_KEY = 'automatic_reminders'
 
 class Preferences(object):
 
 	_current_prefs = None
+
+	@classmethod
+	def sync(cls):
+		from wunderlist.api import settings
+
+		prefs = cls.current_prefs()
+
+		# Only set the value once, otherwise allow it to be managed in
+		# the workflow
+		if prefs._get(AUTOMATIC_REMINDERS_KEY, None) is None:
+			for s in settings.settings():
+				if s['key'] == 'automatic_reminders':
+					# In case the value, currently "on" or "off" is changed to
+					# boolean this logic will still work
+					prefs.automatic_reminders = s['value'] and s['value'] != 'off'
+					break
 
 	@classmethod
 	def current_prefs(cls):
@@ -56,3 +73,11 @@ class Preferences(object):
 	@explicit_keywords.setter
 	def explicit_keywords(self, explicit_keywords):
 		self._set(EXPLICIT_KEYWORDS_KEY, explicit_keywords)
+
+	@property
+	def automatic_reminders(self):
+		return self._get(AUTOMATIC_REMINDERS_KEY, False)
+
+	@automatic_reminders.setter
+	def automatic_reminders(self, automatic_reminders):
+		self._set(AUTOMATIC_REMINDERS_KEY, automatic_reminders)
